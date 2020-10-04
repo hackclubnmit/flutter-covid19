d3dfab370032bc69dca856898c4db861b390d0d9
Janhavi <janhavi1dkulkarni@gmail.com>
2020-10-04 18:58:56 +0530

    Fixed issue #1

--- lib/service/cautio.dart
+++ lib/service/cautio.dart
@@ -36,10 +36,10 @@ class _CautionsState extends State<Cautions> {
           "Clean your hands often"),
       buildSwiperItem(
         "assests/wash.png",
-        "Central",
+        "use a hand sanitizer that contains at least 60% alcohol. Cover all surfaces of your hands and rub them together until they feel dry.",
         Color(0xFFffcc00),
         secondtGradient,
-        "+91-11-23978046"),
+        "Avoid close contact"),
       buildSwiperItem(
           "assests/home.png",
           "Stay home if you are sick, except to get medical care.",
